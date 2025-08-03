244f7bbed45c7b6152b2b8de98d7dd65459eaa4b
Kareem Mamdouh <T1gerEzPro@gmail.com>
2025-08-03 16:31:07 +0300

    add change to ascii home screen

---
+++ lua/plugins/snacksRight.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {},
+    picker = {
+      sources = {
+        explorer = {
+          layout = { layout = { position = "right" } },
+          follow_file = false,
+          jump = { close = false },
+          supports_live = false,
+        },
+      },
+    },
+  },
+}
